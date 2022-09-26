e2b3db4d90f142189df49a8f274384e6442a4f11
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-09-26 10:05:53 +0300

    commit 2

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// SWIPER
+
+new Swiper(".swiper",{
+    pagination: {
+        el:".swiper-pagination",
+        clickable:true,
+    },
+});
+
+
+
+//  MODAL VIDEO
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
